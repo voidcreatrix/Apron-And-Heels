8265de1037bbc24c99865931e13621a8c407f522
Void Creatrix <44763104+voidcreatrix@users.noreply.github.com>
2018-11-12 14:44:25 -0400

    Update media/games/Apron-And-Heels/Apron-And-Heels.js
    added options to sit 2 that lead to "under construction sits

--- media/games/Apron-And-Heels/Apron-And-Heels.js
+++ media/games/Apron-And-Heels/Apron-And-Heels.js
@@ -48,7 +48,8 @@ undum.game.situations = {
         "<h4>Baker</h4>\
 		<p>I can feel you pressing up against me and into the countertop while I measure and pour \
 		and stir. I'm glad I remembered to wear an apron.  This could get messy.</p>\
-		<center><img src='https://www.historicalemporium.com/store/media/001559/001559_04.jpg' alt='apron closeup with Battenburg lace' ></center>",
+		<center><img src='https://www.historicalemporium.com/store/media/001559/001559_04.jpg' alt='apron closeup with Battenburg lace' ></center>\
+		<p class='transient'>Lover, will you <a href='offer_to_help'>offer to help</a>?  Or <a href='get_in_the_way'>get in the way</a>?</p>",
         {
             heading: "Making Winter Treats in the Kitchen",
             tags: ["topic"],
@@ -60,7 +61,34 @@ undum.game.situations = {
             },
         }
     ),
-
+	
+	get_in_the_way: new undum.SimpleSituation(
+        "<h4>Lover</h4>\
+		<p>I love standing behind you while you're baking.  Wrapping my arms around your hips\
+		while you stir something on the stove is one of my favourite winter pick-me-ups. I want\
+		to nuzzle your neck and get in the way.</p>\
+		\
+		<p>I don't know why I'm somehow always exactly in the way of what you're trying to do. \
+		There are so many delicious textures and flavours to try, and all of them are better\
+		licked off of your fingers.  Can I have some of that?\
+		\
+		<p class='transient'>Baker, will you <a href='give_a_taste'>give them a taste</a>? Or <a href='you_can_wait'>tell them to wait</a>?</p>"
+        
+    ),
+	
+	offer_to_help: new undum.SimpleSituation(
+        "<h4>Under construction...</h4>"
+        
+    ),	
+	
+	give_a_taste: new undum.SimpleSituation(
+        "<h4>Under construction...</h4>"
+        
+    ),	
+	you_can_wait: new undum.SimpleSituation(
+        "<h4>Under construction...</h4>"
+        
+    ),	
     // NB: The 'hub' situation which is the main list of topics, is
     // defined wholly in the HTML file, and doesn't have an entry in
     // the game.situations dictionary in this file.
